0df15c1635250125d33b772eada4620c37be0cf1
Bequa La Froth <bequa@eelfroth.com>
2019-11-12 06:45:12 +0100

    gravball

--- tj.c
+++ tj.c
@@ -23,23 +23,23 @@ tcsetattr(0,2,&r);
 fcntl(0,4,fcntl(0,3)|2048);
 struct winsize z;
 ioctl(1,21523,&z);
-int w=z.ws_col,h=z.ws_row,p=w/2,x=8*w/2,y=16,i,d=1,g=3,o;
+int w=z.ws_col,h=z.ws_row,p=w/2,x=8*w/2,y=16,i,d=1,g=0,o;
 p(e"[?1049h"e"[2J"e"[?25l"e"[%d;%dH"e"[s",h,0);
 for(;;){
 i=getchar();
 p=p+(i=='a'?-2:i=='d'?2:0);
 p=p<8?w:p>w?8:p;
-x+=d;
+x+=d/3;
 if(x/8<1||x/8>w){d=-d;p("\a",0,0);}
-y+=g;
-if(y/8<1||y/8>h-1){g=-g;p("\a",0,0);}
+y+=g/4;
+g++;
 o=-4+p-x/8;
-if(y/8>h-1){d-=o*2;s++;if(abs(o)>4)exit(0);}
+if(y/8>h-1){d-=o*2-1;s++;g=-g+abs(o)*2;if(abs(o)>4)exit(0);}
 p(e"[u%*s",p,"(((())))");
 p(e"[%d;%dHO",y/8,x/8);
 p(e"[0;%dH%d",w/2,s+1);
 fflush(0);
-usleep(20000);
+usleep(50000/2);
 p(e"[2J",0,0);
 }
 }
